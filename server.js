154769e3f4bf522422cf44c57acdfba0b5c877d3
tuckl <tuckerlarrabee13@gmail.com>
2022-03-19 14:19:33 -0400

    heroku.2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
-  secret: process.env.COOKIE_SECRET,
+  secret: Supersecretsecret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
